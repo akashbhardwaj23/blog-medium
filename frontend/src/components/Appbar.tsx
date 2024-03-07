1e41e007e9c13bf7743a4c7d99875f17f3687dbe
akash23-ai <akashbhardwaj2024@gmail.com>
2024-03-08 01:55:20 +0530

    Changed the logo and button style

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -4,13 +4,18 @@ import { Avatar } from "./BlogCard";
 function Appbar() {
   return (
     <div className="border-b flex justify-between px-10 py-4">
-      <Link to={"/blogs"} className="flex flex-col justify-center font-semibold cursor-pointer">
-          Medium
+      <Link
+        to={"/blogs"}
+        className="flex flex-col justify-center font-semibold cursor-pointer"
+      >
+        Medium
       </Link>
       <div>
-      <Link to={'/publish'}>
-      <button type="button" className="text-white bg-green-700 mr-4 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">New</button>
-      </Link>
+        <Link to={"/publish"}>
+          <button className="bg-transparent hover:bg-blue-500 mr-4 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            New
+          </button>
+        </Link>
         <Avatar authorName="Akash" size="big" />
       </div>
     </div>
--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -37,7 +37,7 @@ export default BlogCard;
 export function Avatar({ authorName, size = "small" }: { authorName: string, size? : "small" | "big" }) {
   return (
     <div className={`relative inline-flex items-center justify-center ${size === "small"  ? "w-6 h-6" : "w-10 h-10"} overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}>
-      <span className={`font-small ${size === "small" ? "text-xs":"text-sm"} text-gray-600 dark:text-gray-300`}>
+      <span className={`font-medium ${size === "small" ? "text-xs":"text-sm"} text-gray-600 dark:text-gray-300`}>
         {authorName[0]}
       </span>
     </div>
