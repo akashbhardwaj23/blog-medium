539dd816477aebd9edeea249cb8de7b38faf038d
akash23-ai <akashbhardwaj2024@gmail.com>
2024-03-08 19:35:56 +0530

    Fixed some css

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -5,7 +5,7 @@ interface BlogCardProps {
   title: string;
   content: string;
   publishedDate: string;
-  id : string;
+  id: string;
 }
 
 function BlogCard({
@@ -13,31 +13,53 @@ function BlogCard({
   title,
   content,
   publishedDate,
-  id 
+  id,
 }: BlogCardProps) {
   return (
     <Link to={`/blog/${id}`}>
-      <div className="border-b-2 border-solid border-slate-200 p-4 pb-4 cursor-pointer">
-      <div className="flex">
+      <div className="border-b border-solid border-slate-300 p-4 pb-4 cursor-pointer">
+        <div className="flex">
           <Avatar authorName={authorName} />
-        <div className="font-extralight px-2 tetx-sm flex flex-col justify-center">{authorName}</div>
-        <div className="text-[10px] flex justify-center items-center ">&#9679;</div>
-        <div className="pl-2 font-thin text-slate-300 text-sm flex flex-col justify-center">{publishedDate}</div>
+          <div className="font-normal px-2 tetx-sm flex flex-col justify-center">
+            {authorName}
+          </div>
+          <div className="text-[10px] flex justify-center items-center text-slate-600">
+            &#9679;
+          </div>
+          <div className="pl-2 font-extralight text-slate-500 text-sm flex flex-col justify-center">
+            {publishedDate}
+          </div>
+        </div>
+        <div className="text-xl font-semibold pt-2">{title}</div>
+        <div className="text-md font-thin">{content.slice(0, 100) + "..."}</div>
+        <div className="text-slate-500 text-sm font-thin pt-4">{`${Math.ceil(
+          content.length / 100
+        )} minutes`}</div>
       </div>
-      <div className="text-xl font-semibold pt-2">{title}</div>
-      <div className="text-md font-thin">{content.slice(0, 100) + "..."}</div>
-      <div className="text-slate-500 text-sm font-thin pt-4">{`${Math.ceil(content.length / 100)} minutes`}</div>
-    </div>
     </Link>
   );
 }
 
 export default BlogCard;
 
-export function Avatar({ authorName, size = "small" }: { authorName: string, size? : "small" | "big" }) {
+export function Avatar({
+  authorName,
+  size = "small",
+}: {
+  authorName: string;
+  size?: "small" | "big";
+}) {
   return (
-    <div className={`relative inline-flex items-center justify-center ${size === "small"  ? "w-6 h-6" : "w-10 h-10"} overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}>
-      <span className={`font-medium ${size === "small" ? "text-xs":"text-sm"} text-gray-600 dark:text-gray-300 flex items-center justify-center`}>
+    <div
+      className={`relative inline-flex items-center justify-center ${
+        size === "small" ? "w-6 h-6" : "w-10 h-10"
+      } overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}
+    >
+      <span
+        className={`font-medium ${
+          size === "small" ? "text-xs" : "text-sm"
+        } text-gray-600 dark:text-gray-300 flex items-center justify-center`}
+      >
         {authorName[0]}
       </span>
     </div>
