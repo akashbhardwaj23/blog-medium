cc833e829322983ac27f2e99d9b40afb7f5e97f4
akash23-ai <akashbhardwaj2024@gmail.com>
2024-03-09 19:12:07 +0530

    Minor changes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
-// export const BACKEND_URL = "https://backend.akashbhardwaj2024.workers.dev";
+export const BACKEND_URL = "https://backend.akashbhardwaj2024.workers.dev";
 
-export const BACKEND_URL = "http://localhost:8787";
+// export const BACKEND_URL = "http://localhost:8787";
