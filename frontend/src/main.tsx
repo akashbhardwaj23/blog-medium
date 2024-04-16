d92c5ff5eda2a3c1512aefb0bb3d88bc8a9bf834
akash23-ai <akashbhardwaj2024@gmail.com>
2024-04-16 14:05:38 +0530

    Added google-auth

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || process.env.VITE_GOOGLE_CLIENT_ID}>
+   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
          <App />
    </GoogleOAuthProvider>
 )
