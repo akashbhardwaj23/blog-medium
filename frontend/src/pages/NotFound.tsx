7f619757dfb99efe50b96261f001383e5689ab0b
akash23-ai <akashbhardwaj2024@gmail.com>
2024-05-11 16:57:06 +0530

    Added custom 404

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -3,15 +3,40 @@ import { useNavigate } from "react-router-dom";
 function NotFound() {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <div className="text-4xl font-bold mb-4">404 Not Found</div>
+    <div className="h-screen py-4">
+      <div className="flex justify-center items-center pt-8">
+        <img src="/Goodies Hey.png" alt="hey" className="w-26 h-14" />
+      </div>
+      <div className="text-[#4A3993] flex justify-center items-center">
+        <h1 className="font-bold text-base">What are you doing here?!</h1>
+      </div>
+    <div className="text-[14rem] font-bold text-[#FFCF06] flex justify-center items-center">
+      <h1 className="">4</h1>
+      <img src="/Goodies Danger.png" alt="goodies" className="w-60 h-45 relative top-4" />
+      <h1 className="">4</h1>
+    </div>
 
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => {
-        navigate("/blogs");
-      }}>
-        Home
+    <div className="flex justify-center items-center">
+      <button className="border-2 border-gray-700 px-4 py-2 rounded-lg flex" onClick={() => navigate("/")}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+          />
+        </svg>
+
+        <span className="ml-2 text-lg font-semibold">Go Home</span>
       </button>
     </div>
+  </div>
   );
 }
 
